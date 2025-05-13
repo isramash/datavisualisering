761c3c74076d451e6a834a198f23a8a9719181fc
t-engstrom <tyrlaegiehler@gmail.com>
2025-05-13 13:12:45 +0200

    Made test file

--- test.js
+++ test.js
@@ -1,76 +1,76 @@
 const wSvg = 600, hSvg = 400,
-      hViz = .8 * hSvg, wViz = .8 * wSvg,
-      hPadding = (hSvg - hViz) / 2, wPadding = (wSvg - wViz) / 2
-;
+  hViz = .8 * hSvg, wViz = .8 * wSvg,
+  hPadding = (hSvg - hViz) / 2, wPadding = (wSvg - wViz) / 2
+  ;
 
 
 // Get the Dataset
 const managerDataset = [];
 
 for (let manager of Managers) {
-    const managerID = manager.id;
+  const managerID = manager.id;
 
-    const dataset = {
+  const dataset = {
     id: managerID,
     name: manager.name,
     managerEarnings: [],
     nGigs: [],
   };
-  
-   const djIDs = DJs
+
+  const djIDs = DJs
     .filter(dj => dj.managerID === managerID)
     .map(dj => dj.id);
 
-    const managerGigs = Gigs.filter(g => djIDs.includes(g.djID));
+  const managerGigs = Gigs.filter(g => djIDs.includes(g.djID));
 
-//  console.log(managerGigs);
+  //  console.log(managerGigs);
 
-    const earningsPerYearMap = {};
-    const nGigsPerYearMap = {};
-    console.log(earningsPerYearMap);
-    
-    console.log(nGigsPerYearMap);
+  const earningsPerYearMap = {};
+  const nGigsPerYearMap = {};
+  console.log(earningsPerYearMap);
+
+  console.log(nGigsPerYearMap);
 
   for (let gig of managerGigs) {
     const year = new Date(gig.date).getFullYear();
 
     earningsPerYearMap[year] = (earningsPerYearMap[year] || 0) + gig.managerEarnings;
-    
+
     nGigsPerYearMap[year] = (nGigsPerYearMap[year] || 0) + 1;
 
   }
-// console.log(earningsPerYearMap);
+  // console.log(earningsPerYearMap);
 
   for (let year in earningsPerYearMap) {
     const totalEarnings = earningsPerYearMap[year];
 
     dataset.managerEarnings.push({
-        year: parseInt(year),
-        totalEarnings: totalEarnings
-  });
-
-  for (let year in nGigsPerYearMap) {
-    const totalGigs = nGigsPerYearMap[year];
-    dataset.nGigs.push({
       year: parseInt(year),
-      totalGigs: totalGigs
+      totalEarnings: totalEarnings
     });
-  }
-  
-}
 
-//   console.log(earningsPerYearMap);
-  
+    for (let year in nGigsPerYearMap) {
+      const totalGigs = nGigsPerYearMap[year];
+      dataset.nGigs.push({
+        year: parseInt(year),
+        totalGigs: totalGigs
+      });
+    }
+
+  }
+
+  //   console.log(earningsPerYearMap);
+
   managerDataset.push(dataset);
 
-//   console.log(dataset);
-  
+  //   console.log(dataset);
+
 }
 // console.log(managerDataset);
 
 // Förbered en array med alla värden på X-skalan (months)
 let years = [];
-for (let year = 2015; year < 2025; year ++) { years.push(year); }
+for (let year = 2015; year < 2025; year++) { years.push(year); }
 
 console.log(years);
 
@@ -83,7 +83,7 @@ for (let dataset of managerDataset) {
     maxEarnings = Math.max(maxEarnings, point.totalEarnings);
   }
   for (let point of dataset.nGigs) {
-  maxGigs = Math.max(maxGigs, point.totalGigs);
+    maxGigs = Math.max(maxGigs, point.totalGigs);
   }
 
 
@@ -96,25 +96,25 @@ d3.select("main").append("div")
   .enter()
   .append("button")
   .text(d => d.name)
-  .on("click", (event,d) => {
+  .on("click", (event, d) => {
     let line = d3.select(`#line_${d.id}`);
     let points = d3.selectAll(`#points_${d.id} .data-point`);
 
     let gigsLine = d3.select(`#gigsLine_${d.id}`);
     let gigsPoints = d3.selectAll(`#gigsPoints_${d.id} .gigs-point`);
-        
+
     let visible = line.attr("visibility");
     let newValue = (visible == "hidden") ? "visible" : "hidden";
-    
+
     d3.selectAll(".data-line").attr("visibility", "hidden");
     d3.selectAll(".data-point").attr("visibility", "hidden");
-    
+
     d3.selectAll(".gigs-line").attr("visibility", "hidden");
     d3.selectAll(".gigs-point").attr("visibility", "hidden");
 
     let activeButton = document.querySelector("button.active");
     if (activeButton) activeButton.classList.remove("active")
-    
+
     let classAction = newValue == "hidden" ? "remove" : "add";
     event.target.classList[classAction]("active");
 
@@ -126,12 +126,12 @@ d3.select("main").append("div")
   })
 
 const svg = d3.select("main").append("svg")
-            .attr("width", wSvg)
-            .attr("height", hSvg);
+  .attr("width", wSvg)
+  .attr("height", hSvg);
 
 const svg2 = d3.select("main").append("svg")
-            .attr("width", wSvg)
-            .attr("height", hSvg);
+  .attr("width", wSvg)
+  .attr("height", hSvg);
 //Skala
 let xScale = d3.scaleBand().domain(years).range([wPadding, wPadding + wViz]);
 let yScale = d3.scaleLinear().domain([0, maxEarnings]).range([hPadding + hViz, hPadding]);
@@ -142,7 +142,7 @@ svg.append("g")
   .call(d3.axisBottom(xScale))
   .attr("transform", `translate(0, ${hPadding + hViz})`);
 
-  svg2.append("g")
+svg2.append("g")
   .call(d3.axisBottom(xScale))
   .attr("transform", `translate(0, ${hPadding + hViz})`);
 
@@ -151,16 +151,16 @@ svg.append("g")
   .call(d3.axisLeft(yScale))
   .attr("transform", `translate(${wPadding}, 0)`);
 
-  svg2.append("g")
+svg2.append("g")
   .call(d3.axisLeft(yScaleGigs))
   .attr("transform", `translate(${wPadding}, 0)`);
-          
+
 
 const dMaker = d3.line()
   .x(d => xScale(d.year) + xScale.bandwidth() / 2)
-  .y(d => yScale(d.totalEarnings)) 
+  .y(d => yScale(d.totalEarnings))
 
-  const dMakerGigs = d3.line()
+const dMakerGigs = d3.line()
   .x(d => xScale(d.year) + xScale.bandwidth() / 2)
   .y(d => yScaleGigs(d.totalGigs));
 
@@ -179,7 +179,7 @@ svg.append("g")
   .attr("stroke-width", 2)
   .attr("visibility", "hidden");
 
-  // skapa gigs lines
+// skapa gigs lines
 svg2.append("g")
   .selectAll("path")
   .data(managerDataset)
