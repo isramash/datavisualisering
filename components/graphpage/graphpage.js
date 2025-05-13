f71d1ed3fde1c9109baeb2ad803b4b3b399797c3
t-engstrom <tyrlaegiehler@gmail.com>
2025-05-13 14:46:30 +0200

    added graphpage

---
+++ components/graphpage/graphpage.js
@@ -0,0 +1,7 @@
+function graphpage() {
+    let main = document.querySelector("main");
+    let graphpageCon = document.createElement("div");
+    graphpageCon.id = "graphpageCon";
+
+    main.append(graphpageCon);
+}
